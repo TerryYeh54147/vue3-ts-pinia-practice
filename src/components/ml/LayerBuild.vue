2702536b34612507bb4201e58d594ede2115aa1a
terry54147 <0930164@gmail.com>
2022-09-04 00:50:04 +0800

    add remove layer card function

--- src/components/ml/LayerBuild.vue
+++ src/components/ml/LayerBuild.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row q-col-gutter-md">
     <div v-for="(layer, idx) in layers" :key="`Layer_${idx}`" class="col-4">
-        <LayerCard :title="layer.label" />
+      <LayerCard :title="`Layer${idx}`" :data="layer" @del="del" />
     </div>
   </div>
   <q-page-sticky position="bottom-right" :offset="[10, 10]">
@@ -17,7 +17,8 @@ import { Layer } from '@/models/model';
 const layers = reactive<Array<Layer>>([]);
 let globalCnt = ref(0);
 const getNextId = () => {
-  return globalCnt.value++;
+  globalCnt.value++;
+  return globalCnt.value.toString();
 };
 
 const add = () => {
@@ -28,5 +29,12 @@ const add = () => {
   };
   console.log('add');
   layers.push(newItem);
+};
+
+const del = (id: string) => {
+  const targetIdx = layers.findIndex((e) => e.id === id);
+  layers.splice(targetIdx, 1);
+  console.log(`--- remove id: ${id} from layers---`);
+  console.log(layers);
 };
 </script>
--- src/components/ml/LayerCard.vue
+++ src/components/ml/LayerCard.vue
@@ -1,15 +1,34 @@
 <template>
   <q-card>
     <q-card-section>
-      <div class="text-h6 text-blod">{{ title }}</div>
+      <div class="row">
+        <div class="col-11">
+          <div class="text-h6 text-blod">
+            {{ title }}
+          </div>
+        </div>
+        <div class="col-1">
+          <q-btn flat round dense icon="close" color="red" @click="del" />
+        </div>
+      </div>
     </q-card-section>
   </q-card>
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue';
+import { defineProps, defineEmits, PropType, computed } from 'vue';
+import { Layer } from '@/models/model';
 
 const props = defineProps({
   title: String,
+  data: Object as PropType<Layer>,
 });
+
+const emit = defineEmits(['del']);
+const layer = computed(() => props.data);
+
+const del = () => {
+  console.log(`delete ${layer.value?.id}`);
+  emit('del', layer.value?.id);
+};
 </script>
