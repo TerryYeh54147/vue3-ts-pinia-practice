5d3c8a0dcf036395eff24159db36f44ebadad579
terry54147 <0930164@gmail.com>
2022-09-18 00:50:59 +0800

    add env setting component

---
+++ src/components/ml/envSetting.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row justify-around q-col-gutter-md">
+    <div class="col-12 col-md-6">
+      <q-select v-model="setting.framework" :options="options.framework" label="framework" dense />
+    </div>
+    <div class="col-12 col-md-6">
+      <q-toggle v-model="setting.isPretrained" label="isPretrained" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+interface EnvSetting {
+  framework: string;
+  isPretrained: boolean;
+}
+
+const setting = reactive<EnvSetting>({ framework: '', isPretrained: false });
+const options = {
+  framework: ['Tensorflow & Keras'],
+}
+</script>
